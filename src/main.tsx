476a4a575b1d974bbb885afbb56d56e4130fda89
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-03-11 16:56:40 +0000

    Remove comment

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,6 @@ import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
-// const theme = createTheme({ palette: { mode: "dark" } });
 const theme = createTheme({
   colorSchemes: {
     light: true,
