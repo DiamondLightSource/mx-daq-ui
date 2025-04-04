0574b5a8d59f3ece0509ccdaff566a9f1344c87b
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-04-04 11:31:47 +0100

    Add sc-react-ui dependency and start using theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { createTheme, ThemeProvider } from "@mui/material";
+// import { createTheme, ThemeProvider } from "@mui/material";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -7,17 +7,18 @@ import { Provider } from "react-redux";
 import { store } from "@diamondlightsource/cs-web-lib";
 
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider, DiamondTheme } from "@diamondlightsource/sci-react-ui";
 
 const queryClient = new QueryClient();
 
-const theme = createTheme({ palette: { mode: "dark" } });
+// const theme = createTheme({ palette: { mode: "dark" } });
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={DiamondTheme}>
       <Provider store={store}>
         <QueryClientProvider client={queryClient}>
           <App />
