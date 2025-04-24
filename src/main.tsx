8f3be5fc366591bda4f6100627e04fa2f5ceaee4
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-04-24 14:19:22 +0100

    Set up a theme for future needs

--- src/main.tsx
+++ src/main.tsx
@@ -5,25 +5,48 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "@diamondlightsource/cs-web-lib";
+import logo from "./assets/logo.svg";
 
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ThemeProvider, DiamondTheme } from "@diamondlightsource/sci-react-ui";
+import {
+  ThemeProvider,
+  DiamondTheme,
+  ImageColorSchemeSwitchType,
+} from "@diamondlightsource/sci-react-ui";
+import { createTheme, Theme } from "@mui/material";
 
 const queryClient = new QueryClient();
 
-// const theme = createTheme({
-//   colorSchemes: {
-//     light: true,
-//     dark: true,
-//   },
-// });
+// Set up a customisable theme derived from the common DiamondTheme
+// This way it should be possible to easily add settings if required
+const dlsLogo: ImageColorSchemeSwitchType = {
+  src: logo,
+  alt: "DLS Logo",
+};
+
+const I24DiamondTheme: Theme = createTheme({
+  ...DiamondTheme,
+  logos: {
+    normal: dlsLogo,
+  },
+  colorSchemes: {
+    dark: {
+      palette: {
+        background: {
+          default: "121212",
+          paper: "121212",
+        },
+      },
+    },
+  },
+});
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={DiamondTheme}>
+    <ThemeProvider theme={I24DiamondTheme}>
       <Provider store={store}>
         <QueryClientProvider client={queryClient}>
           <App />
