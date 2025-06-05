a519d137f807d05855aee49f7bea87ae68fad000
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-06-05 15:41:02 +0100

    Nevermind

--- src/screens/BlueApiInfo.tsx
+++ src/screens/BlueApiInfo.tsx
@@ -1,14 +1,12 @@
-import { Stack, Box } from "@mui/material";
+import { Stack } from "@mui/material";
 import { WorkerStatus } from "../components/WorkerStatus";
 import { SleepButton } from "../components/SleepButton";
 
 export function BlueApiInfo() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Stack direction="column">
-        <WorkerStatus />
-        <SleepButton />
-      </Stack>
-    </Box>
+    <Stack direction="column">
+      <WorkerStatus />
+      <SleepButton />
+    </Stack>
   );
 }
