bdcc1e4815fa135966108feede2fe653429898bb
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-06-19 14:01:52 +0100

    Add transmission

--- src/screens/BeamlineStats.tsx
+++ src/screens/BeamlineStats.tsx
@@ -12,7 +12,7 @@ export function BeamlineStatsTabPanel() {
   const bgColor = theme.palette.background.paper;
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid2 container spacing={3} justifyContent="center">
+      <Grid2 container spacing={4} justifyContent="center">
         <WorkerStatus />
         <Grid2 size={2} sx={{ bgcolor: bgColor }}>
           <PvComponent
@@ -36,6 +36,14 @@ export function BeamlineStatsTabPanel() {
           <PvComponent
             label="Energy"
             pv="ca://BL24I-MO-DCM-01:ENERGY.RBV"
+            transformValue={parseNumericPv}
+            decimals={4}
+          />
+        </Grid2>
+        <Grid2 size={2} sx={{ bgcolor: bgColor }}>
+          <PvComponent
+            label="Filter Transmission"
+            pv="ca://BL24I-OP-ATTN-01:MATCH"
             transformValue={parseNumericPv}
             decimals={4}
           />
