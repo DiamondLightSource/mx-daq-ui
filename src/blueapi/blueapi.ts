9276338c5ef5d10916e1a288d3822ede120212dd
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-07-24 15:22:00 +0100

    This might work

--- src/blueapi/blueapi.ts
+++ src/blueapi/blueapi.ts
@@ -76,37 +76,56 @@ export function submitPlan(
   }).then((res) => res.json().then((res) => res["task_id"]));
 }
 
+// Note. fetch only rejects a promise on network errors, but http errors
+// must be caught by checking the response
 export function submitTask(
   planName: string,
   planParams: object
-): Promise<object | void> {
+): Promise<string | void> {
   return blueApiCall("/tasks", "POST", {
     name: planName,
     params: planParams,
   }).then((res) => {
     if (!res.ok) {
       throw new Error(
-        `Unable to POST request,response error ${res.statusText}`
+        `Unable to POST request, response error ${res.statusText}`
       );
     }
-    res.json();
+    res.json().then((res) => res["task_id"]);
+  });
+}
+
+export function runTask(taskId: string): Promise<string | void> {
+  return blueApiCall("/worker/task", "PUT", { task_id: taskId }).then((res) => {
+    if (!res.ok) {
+      throw new Error(`Unable to run task, response error ${res.statusText}`);
+    }
+    res.json().then((res) => res["task_id"]);
   });
 }
 
 export function submitAndRunPlanImmediately(
   planName: string,
   planParams: object
-): Promise<string> {
-  return submitPlan(planName, planParams)
-    .then((res) =>
-      // TODO make sure submitPlan was succesful before then putting it to the worker
-      // See https://github.com/DiamondLightSource/mx-daq-ui/issues/17
-      blueApiCall("/worker/task", "PUT", { task_id: res }).then((res) =>
-        res.json().then((res) => res["task_id"])
-      )
-    )
+): Promise<string | void> {
+  return submitTask(planName, planParams)
+    .then((res) => {
+      if (res) {
+        runTask(res);
+      }
+    })
     .catch((error) => console.log(error));
 }
+//   return submitPlan(planName, planParams)
+//     .then((res) =>
+//       // TODO make sure submitPlan was succesful before then putting it to the worker
+//       // See https://github.com/DiamondLightSource/mx-daq-ui/issues/17
+//       blueApiCall("/worker/task", "PUT", { task_id: res }).then((res) =>
+//         res.json().then((res) => res["task_id"])
+//       )
+//     )
+//     .catch((error) => console.log(error));
+// }
 
 export function abortCurrentPlan(): Promise<BlueApiWorkerState> {
   return blueApiCall("/worker/state", "PUT", {
