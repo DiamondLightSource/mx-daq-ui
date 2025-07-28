14fcdeba4551a477e8809456cc7649476e568051
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-07-28 10:27:41 +0100

    Make a custom card for beamline stats

--- src/screens/BeamlineStats.tsx
+++ src/screens/BeamlineStats.tsx
@@ -12,153 +12,154 @@ import { PvComponent } from "../pv/PvComponent";
 import { forceString, parseNumericPv } from "../pv/util";
 import { WorkerStatus } from "../components/WorkerStatus";
 
-type StateBoxProps = { bgColor: string; title: string; titleColor: string };
+type StateBoxProps = {
+  children: React.ReactNode;
+  bgColor?: string;
+  title?: string;
+  titleColor?: string;
+};
+
+function StateCard(props: StateBoxProps) {
+  const bgColor = props.bgColor ? props.bgColor : "#fff";
+  if (props.title) {
+    return (
+      <Card variant="outlined" sx={{ minWidth: 300, bgcolor: bgColor }}>
+        <CardContent>
+          <Typography
+            variant="h1"
+            sx={{
+              color: props.titleColor ? props.titleColor : "#000",
+              fontSize: 18,
+              fontWeight: "fontWeightBold",
+            }}
+          >
+            {props.title}
+          </Typography>
+          {props.children}
+        </CardContent>
+      </Card>
+    );
+  }
+  return (
+    <Card variant="outlined" sx={{ minWidth: 300, bgcolor: bgColor }}>
+      <CardContent>{props.children}</CardContent>
+    </Card>
+  );
+}
+
+function PmacStagesState(props: Omit<StateBoxProps, "children">) {
+  return (
+    <StateCard
+      bgColor={props.bgColor}
+      title={props.title}
+      titleColor={props.titleColor}
+    >
+      <PvComponent
+        label="Stage X"
+        pv="ca://BL24I-MO-CHIP-01:X.RBV"
+        transformValue={parseNumericPv}
+        decimals={4}
+      />
+      <PvComponent
+        label="Stage Y"
+        pv="ca://BL24I-MO-CHIP-01:Y.RBV"
+        transformValue={parseNumericPv}
+        decimals={4}
+      />
+      <PvComponent
+        label="Stage Z"
+        pv="ca://BL24I-MO-CHIP-01:Z.RBV"
+        transformValue={parseNumericPv}
+        decimals={4}
+      />
+    </StateCard>
+  );
+}
+
+function ScanStatus(props: Omit<StateBoxProps, "children">) {
+  return (
+    <StateCard
+      bgColor={props.bgColor}
+      title={props.title}
+      titleColor={props.titleColor}
+    >
+      <PvComponent
+        label="Scan Status"
+        pv="ca://BL24I-MO-STEP-14:signal:P2401"
+        transformValue={forceString}
+      />
+      <PvComponent
+        label="Frames Counter"
+        pv="ca://BL24I-MO-STEP-14:signal:P2402"
+        transformValue={forceString}
+      />
+    </StateCard>
+  );
+}
+
+function BeamlineInfo(props: Omit<StateBoxProps, "children">) {
+  return (
+    <StateCard
+      bgColor={props.bgColor}
+      title={props.title}
+      titleColor={props.titleColor}
+    >
+      <PvComponent
+        label="Flux"
+        pv="ca://BL24I-EA-FLUX-01:XBPM-03"
+        transformValue={parseNumericPv}
+        decimals={2}
+        scaleFactor={1e-9}
+        render={({ label, value }: PvItem) => {
+          return (
+            <Box>
+              <p>
+                <b>{label}:</b> {value}e+09
+              </p>
+            </Box>
+          );
+        }}
+      />
+      <PvComponent
+        label="Energy"
+        pv="ca://BL24I-MO-DCM-01:ENERGY.RBV"
+        transformValue={parseNumericPv}
+        decimals={4}
+      />
+      <PvComponent
+        label="Filter Transmission"
+        pv="ca://BL24I-OP-ATTN-01:MATCH"
+        transformValue={parseNumericPv}
+        decimals={4}
+      />
+    </StateCard>
+  );
+}
+
+function ShuttersState(props: Omit<StateBoxProps, "children">) {
+  return (
+    <StateCard
+      bgColor={props.bgColor}
+      title={props.title}
+      titleColor={props.titleColor}
+    >
+      <PvComponent
+        label="Experiment Shutter"
+        pv="ca://BL24I-PS-SHTR-01:CON"
+        transformValue={forceString}
+      />
+      <PvComponent
+        label="Fast Shutter"
+        pv="ca://BL24I-EA-SHTR-01:STA"
+        transformValue={forceString}
+      />
+    </StateCard>
+  );
+}
 
 // These should be in a permanent sidebar
 // See https://github.com/DiamondLightSource/mx-daq-ui/issues/46
 // Maybe I should leave this for separate PR when I put in drawer ...
-function PmacStagesState(props: StateBoxProps) {
-  return (
-    <Card variant="outlined" sx={{ minWidth: 300, bgcolor: props.bgColor }}>
-      <CardContent>
-        <Typography
-          variant="h1"
-          sx={{
-            color: props.titleColor,
-            fontSize: 18,
-            fontWeight: "fontWeightBold",
-          }}
-        >
-          {props.title}
-        </Typography>
-        <PvComponent
-          label="Stage X"
-          pv="ca://ME14E-MO-CHIP-01:X.RBV"
-          transformValue={parseNumericPv}
-          decimals={4}
-        />
-        <PvComponent
-          label="Stage Y"
-          pv="ca://ME14E-MO-CHIP-01:Y.RBV"
-          transformValue={parseNumericPv}
-          decimals={4}
-        />
-        <PvComponent
-          label="Stage Z"
-          pv="ca://ME14E-MO-CHIP-01:Z.RBV"
-          transformValue={parseNumericPv}
-          decimals={4}
-        />
-      </CardContent>
-    </Card>
-  );
-}
-
-function ScanStatus(props: StateBoxProps) {
-  return (
-    <Card variant="outlined" sx={{ minWidth: 300, bgcolor: props.bgColor }}>
-      <CardContent>
-        <Typography
-          variant="h1"
-          sx={{
-            color: props.titleColor,
-            fontSize: 18,
-            fontWeight: "fontWeightBold",
-          }}
-        >
-          {props.title}
-        </Typography>
-        <PvComponent
-          label="Scan Status"
-          pv="ca://BL24I-MO-STEP-14:signal:P2401"
-          transformValue={forceString}
-        />
-        <PvComponent
-          label="Frames Counter"
-          pv="ca://BL24I-MO-STEP-14:signal:P2402"
-          transformValue={forceString}
-        />
-      </CardContent>
-    </Card>
-  );
-}
-
-function BeamlineInfo(props: StateBoxProps) {
-  return (
-    <Card variant="outlined" sx={{ minWidth: 300, bgcolor: props.bgColor }}>
-      <CardContent>
-        <Typography
-          variant="h1"
-          sx={{
-            color: props.titleColor,
-            fontSize: 18,
-            fontWeight: "fontWeightBold",
-          }}
-        >
-          {props.title}
-        </Typography>
-        <PvComponent
-          label="Flux"
-          pv="ca://BL24I-EA-FLUX-01:XBPM-03"
-          transformValue={parseNumericPv}
-          decimals={2}
-          scaleFactor={1e-9}
-          render={({ label, value }: PvItem) => {
-            return (
-              <Box>
-                <p>
-                  <b>{label}:</b> {value}e+09
-                </p>
-              </Box>
-            );
-          }}
-        />
-        <PvComponent
-          label="Energy"
-          pv="ca://BL24I-MO-DCM-01:ENERGY.RBV"
-          transformValue={parseNumericPv}
-          decimals={4}
-        />
-        <PvComponent
-          label="Filter Transmission"
-          pv="ca://BL24I-OP-ATTN-01:MATCH"
-          transformValue={parseNumericPv}
-          decimals={4}
-        />
-      </CardContent>
-    </Card>
-  );
-}
-
-function ShuttersState(props: StateBoxProps) {
-  return (
-    <Card variant="outlined" sx={{ minWidth: 300, bgcolor: props.bgColor }}>
-      <CardContent>
-        <Typography
-          variant="h1"
-          sx={{
-            color: props.titleColor,
-            fontSize: 18,
-            fontWeight: "fontWeightBold",
-          }}
-        >
-          {props.title}
-        </Typography>
-        <PvComponent
-          label="Experiment Shutter"
-          pv="ca://BL24I-PS-SHTR-01:CON"
-          transformValue={forceString}
-        />
-        <PvComponent
-          label="Fast Shutter"
-          pv="ca://BL24I-EA-SHTR-01:STA"
-          transformValue={forceString}
-        />
-      </CardContent>
-    </Card>
-  );
-}
-
 export function BeamlineStatsTabPanel() {
   const theme = useTheme();
   const bgColor = theme.palette.background.paper;
