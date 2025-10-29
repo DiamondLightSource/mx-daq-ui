cb40a5d87639ea8d5732e428b1c717fb72d50035
Tamoor Shahid <tamoor.shahid@diamond.ac.uk>
2025-10-29 11:14:58 +0000

    Added config for absolute pathing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     "process.env": {
       VITE_PVWS_SOCKET: "pvws.diamond.ac.uk",
       VITE_PVWS_SSL: "true",
+    },
+  },
+  resolve: {
+    alias: {
+      "": path.resolve(__dirname, "./src"),
     },
   },
   base: "/mx-daq-ui/",
