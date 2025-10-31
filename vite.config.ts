7c75c58095e6159eaf60886e2322b5f863d76a6d
Tamoor Shahid <tamoor.shahid@diamond.ac.uk>
2025-10-31 14:41:36 +0000

    Added more aliases instead of one generic alias

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,12 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "": path.resolve(__dirname, "./src"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      blueapi: path.resolve(__dirname, "./src/blueapi"),
+      components: path.resolve(__dirname, "./src/components"),
+      pv: path.resolve(__dirname, "./src/pv"),
+      routes: path.resolve(__dirname, "./src/routes"),
+      screens: path.resolve(__dirname, "./src/screens"),
     },
   },
   base: "/mx-daq-ui/",
