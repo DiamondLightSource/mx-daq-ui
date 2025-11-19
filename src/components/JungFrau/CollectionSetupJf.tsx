236143232e70908d6407031fa04bc47958f70faf
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-11-19 16:32:27 +0000

    Finish filling everything in

--- src/components/JungFrau/CollectionSetupJf.tsx
+++ src/components/JungFrau/CollectionSetupJf.tsx
@@ -6,14 +6,78 @@ import {
   Typography,
   useTheme,
 } from "@mui/material";
+import { AbortButton, RunPlanButton } from "blueapi/BlueapiComponents";
 import { ParameterInput } from "components/ParameterInputs";
 import { JungfrauRotationContext } from "context/jungfrau/JungfrauRotationContext";
+import React from "react";
 import { useContext, useState } from "react";
 
 function fullStorageDirectory(visit: string, subDir: string): string {
   const date = new Date();
   const year = date.getFullYear();
   return `/dls/i24/data/${year}/${visit}/jungfrau/${subDir}`;
+}
+
+function RunButtons({
+  visit,
+  storageDirectory,
+}: {
+  visit: string;
+  storageDirectory: string;
+}): JSX.Element {
+  const {
+    expTime,
+    detDist,
+    fileName,
+    shutterOpenT,
+    omegaStart,
+    omegaIncrement,
+    scanWidth,
+    transFract,
+  } = useContext(JungfrauRotationContext);
+  return (
+    <React.Fragment>
+      <Stack direction={"row"} spacing={4} justifyContent={"center"}>
+        <RunPlanButton
+          btnLabel="Run single rotation"
+          planName="run_single_rotation_plan"
+          planParams={{
+            exposure_time_s: expTime,
+            omega_start_deg: omegaStart,
+            omega_increment_deg: omegaIncrement,
+            total_scan_width_deg: scanWidth,
+            detector_distance_mm: detDist,
+            visit: visit,
+            file_name: fileName,
+            storage_directory: storageDirectory,
+            shutter_opening_time_s: shutterOpenT,
+            transmission: transFract[0],
+          }}
+          title="Run single rotation scan"
+          btnSize="large"
+        />
+        <RunPlanButton
+          btnLabel="Run multiple rotations"
+          planName="run_multi_rotation_plan"
+          planParams={{
+            exposure_time_s: expTime,
+            omega_start_deg: omegaStart,
+            omega_increment_deg: omegaIncrement,
+            total_scan_width_deg: scanWidth,
+            detector_distance_mm: detDist,
+            visit: visit,
+            file_name: fileName,
+            storage_directory: storageDirectory,
+            shutter_opening_time_s: shutterOpenT,
+            transmission_fractions: transFract,
+          }}
+          title="Run multiple rotation scans ar different transmissions"
+          btnSize="large"
+        />
+        <AbortButton />
+      </Stack>
+    </React.Fragment>
+  );
 }
 
 export function CollectionSetupJf() {
@@ -23,7 +87,7 @@ export function CollectionSetupJf() {
   const storageDirectory = fullStorageDirectory(visit, context.directory);
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Stack direction={"column"} alignItems={"center"} spacing={2}>
+      <Stack direction={"column"} alignItems={"center"} spacing={3}>
         <Typography
           variant="h2"
           sx={{
@@ -103,6 +167,15 @@ export function CollectionSetupJf() {
             tooltip="Total scan width, in deg"
           />
         </Grid>
+        <Grid container spacing={2} marginTop={3} justifyContent={"center"}>
+          <ParameterInput
+            value={context.transFract}
+            onSet={context.setTransFract}
+            label="Transmission (fraction)"
+            tooltip="Request transmission value(s) for collection, expressed as a fraction. If running a single rotation, just input one value, if running multiples please pass a list."
+          />
+        </Grid>
+        <RunButtons visit={visit} storageDirectory={storageDirectory} />
       </Stack>
     </Box>
   );
