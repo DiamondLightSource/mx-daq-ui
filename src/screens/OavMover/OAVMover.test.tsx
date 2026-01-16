73c47dea3fbf1c20d451699294f1bcd3c90b16dc
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-01-16 11:04:34 +0000

    Fix test

--- src/screens/OavMover/OAVMover.test.tsx
+++ src/screens/OavMover/OAVMover.test.tsx
@@ -3,13 +3,18 @@ import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "@diamondlightsource/cs-web-lib";
 import "@testing-library/jest-dom/vitest";
-import { usePvwsConfig } from "../../config.ts";
 import { OAVSideBar } from "./OAVSideBar";
+
+const defaultConfig = {
+  PVWS_SOCKET: "pvws.diamond.ac.uk",
+  PVWS_SSL: true,
+  THROTTLE_PERIOD: 100,
+};
 
 describe("OavMover Components", () => {
   it("should display all of the various components ", () => {
     render(
-      <Provider store={store(usePvwsConfig())}>
+      <Provider store={store(defaultConfig)}>
         <OAVSideBar />
       </Provider>,
     );
