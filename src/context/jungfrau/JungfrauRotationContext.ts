dbff12c3f463035b78e68aa9d30f71d025e32ded
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-01-21 16:30:31 +0000

    Update JF context

--- src/context/jungfrau/JungfrauRotationContext.ts
+++ src/context/jungfrau/JungfrauRotationContext.ts
@@ -4,24 +4,20 @@ import { createContext } from "react";
 
 // changeble only by staff. For now just a text field
 export type JungfrauRotationContextType = {
-  directory: string;
   fileName: string;
   expTime: number;
   detDist: number;
-  shutterOpenT: number;
   transFract: number[];
   omegaStart: number;
   omegaIncrement: number;
-  scanWidth: number;
-  setDirectory: React.Dispatch<React.SetStateAction<string>>;
+  sampleId: number;
   setFileName: React.Dispatch<React.SetStateAction<string>>;
   setExpTime: React.Dispatch<React.SetStateAction<number>>;
   setDetDist: React.Dispatch<React.SetStateAction<number>>;
-  setShutterOpenT: React.Dispatch<React.SetStateAction<number>>;
   setTransFract: React.Dispatch<React.SetStateAction<number[]>>;
   setOmegaStart: React.Dispatch<React.SetStateAction<number>>;
   setOmegaIncrement: React.Dispatch<React.SetStateAction<number>>;
-  setScanWidth: React.Dispatch<React.SetStateAction<number>>;
+  setSampleId: React.Dispatch<React.SetStateAction<number>>;
 };
 
 export const JungfrauRotationContext =
--- src/context/jungfrau/JungfrauRotationProvider.tsx
+++ src/context/jungfrau/JungfrauRotationProvider.tsx
@@ -6,37 +6,31 @@ export const JungfrauRotationProvider = ({
 }: {
   children: ReactNode;
 }) => {
-  const [directory, setDirectory] = useState<string>("test");
-  const [fileName, setFileName] = useState<string>("thau");
+  const [fileName, setFileName] = useState<string>("jf_rotation");
   const [expTime, setExpTime] = useState<number>(0.001);
   const [detDist, setDetDist] = useState<number>(200);
-  const [shutterOpenT, setShutterOpenT] = useState<number>(0.25);
   const [transFract, setTransFract] = useState<number[]>([0.5]);
   const [omegaStart, setOmegaStart] = useState<number>(0);
   const [omegaIncrement, setOmegaIncrement] = useState<number>(0.1);
-  const [scanWidth, setScanWidth] = useState<number>(360);
+  const [sampleId, setSampleId] = useState<number>(0);
 
   return (
     <JungfrauRotationContext.Provider
       value={{
-        directory,
         fileName,
         expTime,
         detDist,
-        shutterOpenT,
         transFract,
         omegaStart,
         omegaIncrement,
-        scanWidth,
-        setDirectory,
+        sampleId,
         setFileName,
         setExpTime,
         setDetDist,
-        setShutterOpenT,
         setTransFract,
         setOmegaStart,
         setOmegaIncrement,
-        setScanWidth,
+        setSampleId,
       }}
     >
       {children}
