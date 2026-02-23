e0ace88aa9b96712889ee9a0e282bace2a23b434
Tamoor Shahid <tamoor.shahid@diamond.ac.uk>
2026-02-23 14:25:29 +0000

    Fixed merge error

--- src/screens/OavMover/OAVStageController.tsx
+++ src/screens/OavMover/OAVStageController.tsx
@@ -12,8 +12,6 @@ const DISPLAY_CONFIG_ENDPOINT =
   "/dls_sw/i24/software/daq_configuration/domain/display.configuration";
 
 export function OavMover() {
-  const DISPLAY_CONFIG_ENDPOINT =
-    "/dls_sw/i24/software/daq_configuration/domain/display.configuration";
   const beamCenterQuery = useConfigCall(DISPLAY_CONFIG_ENDPOINT);
   const currentZoomValue = String(
     useParsedPvConnection({
