825a4d4ffb3214399cf8b29d6e683ee45a6855fe
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-02-24 11:48:02 +0000

    Change where the slash is for reasons

--- src/components/JungFrau/jfUtils.ts
+++ src/components/JungFrau/jfUtils.ts
@@ -1,9 +1,9 @@
 export function fullStorageDirectory(currentVisit: string): string {
-  return `${currentVisit}/jungfrau/`;
+  return `${currentVisit}jungfrau`;
 }
 
 export function getCurrentVisit(instrumentSession: string): string {
   const date = new Date();
   const year = date.getFullYear();
-  return `/dls/i24/data/${year}/${instrumentSession}`;
+  return `/dls/i24/data/${year}/${instrumentSession}/`;
 }
