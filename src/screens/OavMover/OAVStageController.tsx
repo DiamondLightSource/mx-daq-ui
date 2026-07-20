307ef48b6fd04d865eea6af72da35c1b689da0e0
Tamoor <tamoor_shahid@hotmail.com>
2026-07-20 12:06:13 +0100

    86 move on click (#115)
    * Fixed display_config endpoint and moved function out of jsx
    
    * Cleaned up click canvas code with new plan
    
    ---------
    
    Co-authored-by: Tamoor Shahid <tamoor.shahid@diamond.ac.uk>

--- src/screens/OavMover/OAVStageController.tsx
+++ src/screens/OavMover/OAVStageController.tsx
@@ -40,54 +40,35 @@ export function OavMover() {
     return [Number(xLine.split(" ")[2]), Number(yLine.split(" ")[2])];
   }, [beamCenterQuery.data, zoomIndex]);
 
-  // #Issue 86: Remove these constants - https://github.com/DiamondLightSource/mx-daq-ui/issues/86
-  const pixelsPerMicron = 1.25;
   const theme = useTheme();
   const bgColor = theme.palette.background.paper;
 
   const fullVisit = readVisitFromPv();
 
-  return (
-    <Grid2 container spacing={2} columns={12}>
-      <Grid2 size={{ xs: 12, lg: 8 }} sx={{ bgcolor: bgColor }}>
-        <OavVideoStream
-          pv="ca://BL24I-DI-OAV-01:"
-          label="I24 OAV image stream"
-          crosshairX={crosshairX}
-          crosshairY={crosshairY}
-          onCoordClick={(x: number, y: number) => {
-            const [x_um, y_um] = [x / pixelsPerMicron, y / pixelsPerMicron];
-            console.log(
-              `Clicked on position (${x}, ${y}) (px relative to beam centre) in original stream. Relative position in um (${x_um}, ${y_um}). Submitting to BlueAPI...`,
-            );
-            const [x_int, y_int] = [Math.round(x), Math.round(y)];
-            if (Number.isNaN(x_um) || Number.isNaN(y_um)) {
-              console.log("Not submitting plan while disconnected from PVs!");
-            } else {
-              // This is an example but not useful for actual production use.
-              submitAndRunPlanImmediately({
-                planName: "gui_gonio_move_on_click",
-                planParams: { position_px: [x_int, y_int] },
-                instrumentSession: parseInstrumentSession(fullVisit),
-              }).catch((error) => {
-                console.log(
-                  `Failed to run plan gui_gonio_move_on_click, see console and logs for full error. Reason: ${error}`,
-                );
-              });
-            }
-          }}
-        />
-      </Grid2>
+  function onCoordClick(x: number, y: number) {
+    submitAndRunPlanImmediately({
+      planName: "move_on_oav_view_click",
+      planParams: { position: [x, y] },
+      instrumentSession: parseInstrumentSession(fullVisit),
+    }).catch((error) => {
+      console.log(
+        `Failed to run plan , see console and logs for full error. Reason: ${error}`,
+      );
+    });
+  }
 
-      <Grid2
-        size={{ xs: 12, lg: 4 }}
-        sx={{
-          height: "95vh", // Height set to 95vh to span height of screen but to also leave 5vh space for the top navigation header.
-          overflowY: "auto",
-          padding: 2,
-          boxSizing: "border-box",
-        }}
-      >
+  return (
+    <div>
+      <Grid2 container spacing={2} columns={12}>
+        <Grid2 size={9} sx={{ bgcolor: bgColor }}>
+          <OavVideoStream
+            pv="ca://BL24I-DI-OAV-01:"
+            label="I24 OAV image stream"
+            crosshairX={crosshairX}
+            crosshairY={crosshairY}
+            onCoordClick={onCoordClick}
+          />
+        </Grid2>
         <OAVSideBar />
       </Grid2>
     </Grid2>
